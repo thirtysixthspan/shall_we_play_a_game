e478be5966d824448109deff13bd5f6946160485
Derrick Parkhurst <derrick.parkhurst@gmail.com>
2012-09-09 17:03:53 -0500

    initialize and test game framework

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,17 @@
+require './lib/board.rb'
+require './lib/rule.rb'
+require './lib/player.rb'
+
 class Game
+
+	attr_accessor :name, :players, :rules, :board
+
+	def initialize(params = {})
+		@name = params[:name] || 'Game'
+		@players = params[:players] || [Player.new()]
+		@rules = params[:rules] || []
+  		@board = params[:board] || Board.new(10,10)
+  	end	
 
 end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,5 +2,23 @@ require './spec/spec_helper.rb'
 
 describe Game do
 
+	before :all do
+		@game = Game.new()
+	end
+
+	it "should have a list of players" do
+		@game.players.should be_a_kind_of Array
+		@game.players.size.should be >=1 
+	end
+
+	it "should have a board" do
+		@game.board.should be_a_kind_of Board
+	end
+
+	it "should have a set of rules" do
+		@game.rules.should be_a_kind_of Array
+	end
+
+
 end
 
