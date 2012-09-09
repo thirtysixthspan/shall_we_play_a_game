fb4c51673bdc5c69246ea74a2566d771fbe8111e
Derrick Parkhurst <derrick.parkhurst@gmail.com>
2012-09-09 18:12:14 -0500

    initialize piece and tests

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -2,5 +2,59 @@ require './spec/spec_helper'
 
 describe Piece do
 
+	context "by default" do
+
+		before :all do
+			@piece = Piece.new()
+		end
+
+		it "should have no position" do
+			@piece.position.should == nil
+		end
+
+		it "should have a color of :white" do
+			@piece.color.should == :white
+		end
+
+		it "should have a role of :uno" do
+			@piece.role.should == :uno
+		end
+
+		it "should have no name" do
+			@piece.name.should == nil
+		end	
+
+	end
+
+	context "when configured" do
+
+		before :all do
+			@piece = Piece.new( :name => 'N',
+								:role => :knight,
+								:color => :black,
+								:position => 'QN1')
+		end
+
+		it "should have a position" do
+			@piece.position.should be_a_kind_of String
+			@piece.position.should == 'QN1'
+		end
+
+		it "should have a color of :black" do
+			@piece.color.should == :black
+		end
+
+		it "should have a role of :knight" do
+			@piece.role.should == :knight
+		end
+
+		it "should have a name of N" do
+			@piece.name.should == 'N'
+		end			
+
+	end
+
+
+
 end
 
