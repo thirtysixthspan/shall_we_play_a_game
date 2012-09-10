1b24f2f3d7218ebbd1743edc69029966b95bcc33
Derrick Parkhurst <derrick.parkhurst@gmail.com>
2012-09-09 19:23:57 -0500

    added configured player test with mocks

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,21 +2,52 @@ require './spec/spec_helper'
 
 describe Player do
 
-	before :all do
-		@player = Player.new()
+	context "by default" do
+
+		before :all do
+			@player = Player.new()
+		end
+
+		it "should have a default name of 'Jane Doe'" do
+			@player.name.should == 'Jane Doe'
+		end
+
+		it "should have a default piece color of :white" do
+			@player.color.should == :white
+		end
+
+		it "should by default have an empty set of playing pieces" do
+			@player.pieces.should be_a_kind_of Array
+			@player.pieces.size.should == 0
+		end
+
 	end
 
-	it "should have a default name of 'Jane Doe'" do
-		@player.name.should == 'Jane Doe'
-	end
+	context "when configured" do
 
-	it "should have a default piece color of :white" do
-		@player.color.should == :white
-	end
+		before :each do
+			pieces = []
+			pieces << double(:kind_of? => Piece, :color => :black, :role => :knight, :position => 'QN1')
+			pieces << double(:kind_of? => Piece, :color => :black, :role => :knight, :position => 'KN1')
+			@player = Player.new(:name => 'John Doe',
+								 :color => :black,
+								 :pieces => pieces)
+		end
 
-	it "should by default have an empty set of playing pieces" do
-		@player.pieces.should be_a_kind_of Array
-		@player.pieces.size.should == 0
+		it "should have a name of 'John Doe'" do
+			@player.name.should == 'John Doe'
+		end
+
+		it "should have a color of :black" do
+			@player.color.should == :black
+		end
+
+		it "should by have two knights" do
+			@player.pieces.should be_a_kind_of Array
+			@player.pieces.size.should == 2
+			@player.pieces.select{ |p| p.role == :knight }.size.should == 2
+		end
+
 	end
 
 end
