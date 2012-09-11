786a72d7b8e095620db0779726373a909f8f1c09
Derrick Parkhurst <derrick.parkhurst@gmail.com>
2012-09-10 22:41:28 -0500

    refactor so pieces hold board Positions necessary to evaluate move_to? query

--- lib/piece.rb
+++ lib/piece.rb
@@ -2,6 +2,10 @@ class Piece
 
 	attr_accessor :role, :name, :position, :color, :moves
 
+    def move_to?(new_position)
+    	@moves.select{ |move| @position.along(move[0..1]) == new_position}.size > 0
+    end
+
 	def initialize(params = {})
 		@name = params[:name] if params.include? :name
 		@position = params[:position] if params.include? :position
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -31,42 +31,47 @@ describe Piece do
 
 	context "when configured" do
 
-		before :all do
-			moves = [ [+1,+2,3], [+2,+1,3],
-					  [-1,+2,3], [-2,+1,3],
-					  [+1,-2,3], [+2,-1,3],
-					  [-1,-2,3], [-2,-1,3] ]
+		before :each do
+			moves = [ [+1,0,3], [+2,+0,3] ]
 
-			@piece = Piece.new( :name => 'N',
-								:role => :knight,
+			@position1 = double(:kind_of? => Piece)		  	
+			@position2 = double(:kind_of? => Piece, :paths => {[+1,+2] => @position1})	  	
+			@position2.stub(:along).and_return(@position1)
+
+			@piece = Piece.new( :name => 'P',
+								:role => :pawn,
 								:color => :black,
-								:position => 'QN1',
+								:position => @position2,
 								:moves => moves)
 		end
 
 		it "should have a position" do
-			@piece.position.should be_a_kind_of String
-			@piece.position.should == 'QN1'
+			@piece.position.should be_a_kind_of Piece
+			@piece.position.should == @position2
 		end
 
 		it "should have a color of :black" do
 			@piece.color.should == :black
 		end
 
-		it "should have a role of :knight" do
-			@piece.role.should == :knight
+		it "should have a role of :pawn" do
+			@piece.role.should == :pawn
 		end
 
-		it "should have a name of N" do
-			@piece.name.should == 'N'
+		it "should have a name of P" do
+			@piece.name.should == 'P'
 		end			
 
-		it "should have 8 moves" do
-			@piece.moves.size.should == 8
+		it "should have 2 moves" do
+			@piece.moves.size.should == 2
 		end
 
 		it "should have moves that are triples" do
 			@piece.moves.map{|m| m.size == 3}.inject(:&).should == true
+		end
+
+		it "should permit moving to position1 from position2" do
+			@piece.move_to?(@position1)	
 		end
 
 	end
